61d5b421e0e5a9a28afb6b355f8a5c49fe5874d7
invalid <invalid@invalid>
2013-03-02 22:08:38 -0500

    non-working futures option pricing

--- hw2/binopt.jl
+++ hw2/binopt.jl
@@ -62,5 +62,103 @@ end
 priceoption(0.25, 15, 100, 110, 0.3, 0.02, 0.01, "call", "american") # 2.6040
 
 #q2
-priceoption(0.25, 15, 100, 110, 0.3, 0.02, 0.01, "put", "american")
+priceoption(0.25, 15, 100, 110, 0.3, 0.02, 0.01, "put", "american") #  (12.359784797284926,5)
 #priceoption(0.25, 15, 100, 110, 0.3, 0.02, 0.01, "put", "european")
+
+#q6
+function pricefuturesoption(duration, nperiods, nperiods_future, init_price, strike,
+                            volatility, rate, dividend, putorcall, americaneuro)
+    if putorcall != "call" && putorcall != "put"
+        error("putorcall must be either 'call' or 'put'")
+    end
+    iscall = putorcall == "call"
+    call = iscall ? 1 : -1
+    if americaneuro != "european" && americaneuro != "american"
+        error("putorcall must be either 'call' or 'put'")
+    end
+    isamerican = americaneuro == "american"
+
+    dt = duration/nperiods
+    # x = fill(strike, (1,2^nperiods))
+    discount = exp(rate*dt)
+    invdiscount = exp(-rate*dt)
+    # approx difference between discount and dividend
+    drc = 1.0 + dt*(rate - dividend)
+    up = exp(volatility*sqrt(dt))
+    down = 1/up
+    q = (exp((rate - dividend)*dt) - down)/(up-down) # risk neutral probabilities
+
+    println(up)
+    println(q)
+    println(discount)
+
+    futuresprice = zeros(nperiods_future + 1, nperiods_future + 1)
+    optionval = zeros(nperiods + 1, nperiods + 1)
+    stockval =  zeros(nperiods_future + 1, nperiods_future + 1)
+    stockval[1,1] = init_price
+
+    for i = 2:nperiods_future+1
+        for j = 1:i-1
+            stockval[i,j] = stockval[i-1,j]*down 
+        end
+        stockval[i,i] = stockval[i-1,i-1]*up
+    end
+    
+    ## compute futures price lattice
+    for i = 0:nperiods_future
+        futuresprice[nperiods_future+1,i+1] = stockval[nperiods_future+1,i+1]
+    end
+
+    for i = nperiods_future:-1:1
+        for j = 1:i
+            futuresprice[i,j] = ((1-q)*futuresprice[i+1,j] + q*futuresprice[i+1,j+1])
+        end
+    end
+
+    for j = 0:nperiods
+        optionval[nperiods + 1, j+1] = max((futuresprice[nperiods + 1, j+1] - strike)*call, 0)
+    end
+
+    ## term = zeros(2,nperiods+1)          # value of security and value of option
+    ## for i = 0:nperiods
+    ##     prob = q^i*(1-q)^(nperiods - i)
+    ##     coeff = binomial(nperiods, i)
+    ##     term[1,i+1] = init_price*(up^i)*down^(nperiods-i) # security val
+    ##     term[2,i+1] = max((futuresprice[nperiods+1,i+1] - strike)*call, 0) # option val 
+    ## end
+
+    ## american
+    earliest = nperiods
+    for i = nperiods:-1:1
+        for j = 1:i
+            ## println(optionval[nperiods+1,2])
+            ## println(optionval[nperiods+1,1])
+            ## println(optionval[nperiods,1])
+            ## println(((1-q)*optionval[nperiods+1,1] + q*optionval[nperiods+1,2])*invdiscount)
+            ov = ((1-q)*optionval[i+1,j] + q*optionval[i+1,j+1])*invdiscount
+            fp = futuresprice[i,j]
+            if isamerican
+                optionval[i,j] = max(ov, (fp - strike)*call)
+            else
+                optionval[i,j] = ov
+            end
+            if optionval[i,j] > ov    # exercised
+                earliest = i-1
+            end
+        end
+        #println(term[:,1:i])
+    end
+    return optionval[1,1], earliest, futuresprice, stockval, optionval
+end
+
+
+# Test case
+# optionprice, earliest, futures, stock, optionval = pricefuturesoption(0.5, 10, 10, 100, 100, 0.20, 0.02, 0.01, "put", "european") # 5.212
+
+# optionprice, earliest, futures, stock, optionval = pricefuturesoption(0.25, 10, 15, 100, 110, 0.30, 0.02, 0.01, "call", "american")
+
+optionprice, earliest, futures, stock, optionval = pricefuturesoption(0.25, 1, 2, 100, 110, 0.30, 0.02, 0.01, "call", "american")
+flipud(transpose(stock))
+flipud(transpose(futures))
+flipud(transpose(optionval))
+
