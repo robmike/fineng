c5f5fd34750eb326b4da78a23543dee9fb2546a5
invalid <invalid@invalid>
2013-03-31 21:10:20 -0400

    hw5: q2

--- hw5.py
+++ hw5.py
@@ -2,6 +2,7 @@ import random
 import copy
 from pprint import pprint
 import itertools as it
+import functools as ft
 
 import pdb, sys
 
@@ -68,37 +69,61 @@ def iterate(f, x):
 def latticeiterate(f, init, depth=None):
     itr = iterate(f, init)
     if depth:
-        return [x for x in it.islice(itr, depth)]
+        itr = it.islice(itr, depth)
     else:
-        takewhile(itr)
+        itr = it.takewhile(lambda x: x, itr)
+    return [x for x in itr]
 
 def fupdn(x, up, dn):
     return [dn*y for y in x] + [up*x[-1]]
 
 # takes forward values and short rates zipped
-def fforeq(x, rl):
+def felemprice(x, rl):
     def f(p,r):
-        return (0.5*p)/(1+r)
+        return (0.5*p)/(1+r)    # q assumed 0.5
     p = x
     r = rl[len(p) - 1]
     return [f(wl, rl) + f(wh, rh) for wl,rl,wh,rh in zip([0] + p, [0] + r, p + [0], r + [0])]
 
+# backwards induction using risk neutral pricing
+def frisknetprice(x, rl, q=0.5):
+    r = rl[len(x) - 2]
+    return [(q*wl + (1-q)*wh)/(1+ri) for wl,wh,ri in zip(x[:-1], x[1:], r)]
+
 up = 1.1
 down = 0.9
-q = 0.5
+# q = 0.5 # assumed to be 0.5 in various parts of the code
 nperiod = 10
 r0 = 0.05
+bondprice = 100
 
 # up = 1.25
 # down = 0.9
+# r0 = 0.06
+# nperiod = 6
+# bondprice = 110
 # q = 0.5
 
 shortrates = latticeiterate(lambda x: fupdn(x, up, down), [r0], nperiod+1)
-fwdrates = latticeiterate(lambda x: fforeq(x, shortrates), [1], nperiod+1)
+elemprices = latticeiterate(lambda x: felemprice(x, shortrates), [1], nperiod+1)
 print_lattice(shortrates)
-print_lattice(fwdrates)
+print_lattice(elemprices)
 
 # q1
 print("\n")
-print("%.2f" % (sum(fwdrates[10])*100))
+print("q1")
+#print("%.2f" % (sum(elemprices[10])*bondprice)) # 61.62
 
+# q2
+print("\n")
+print("q2")
+
+bond4lat = latticeiterate(lambda x: frisknetprice(x, shortrates), (4+1)*[bondprice])[::-1]
+bond10lat = latticeiterate(lambda x: frisknetprice(x, shortrates), (nperiod+1)*[bondprice])[::-1]
+# for zero-coupon fwdplat and bond10lat are the same for the periods they overlap
+fwdplat = latticeiterate(lambda x: frisknetprice(x, shortrates), bond10lat[4])[::-1]
+print_lattice(fwdplat)
+print_lattice(bond4lat)
+print_lattice(bond10lat)
+print("%.2f" % (100*fwdplat[0][0]/bond4lat[0][0]))
+#print("%.2f" % (bondprices[0][0]/sum(elemprices[4])))
