259753e25537b82e9516a1b0eaf538a9e62c3d8c
invalid <invalid@invalid>
2013-04-20 15:24:25 -0400

    hw8: correct replication of demo results

--- hw8/hw8.jl
+++ hw8/hw8.jl
@@ -6,77 +6,56 @@ Pkg.add("Distributions")
 using Distributions
 
 sp = read_table("pset8.csv");            # simulated estimates of stock prices
-
-N = 50
-T = 0.25
+N = 50;
+T = 0.25;
 ## volatility
-x1 = sp[:,2]
+x1 = sp[:,2];
 vol = round(100*sqrt(N/T)*std(log(x1[2:]./x1[1:end-1])),2)
 
 df = read_table("pset8-1.csv");            # actual stock holdings and prices
 
+function blackscholes(price, strike, sd, expiration, riskfree_rate, coupon_rate)
+    d1 = log(price/strike) + (riskfree_rate - coupon_rate + sd^2/2)*expiration;
+    d1 = d1./(sd*sqrt(expiration));
+    d2 = d1 - sd*sqrt(expiration)
+    price.*exp(-coupon_rate*expiration).*cdf(Normal(0, 1), d1) -
+    strike*exp(-riskfree_rate*expiration).*cdf(Normal(0, 1), d2)
+end
+
 function delta(price, strike, sd, expiration, riskfree_rate, coupon_rate)
-    d1 = log(price/strike) + (riskfree_rate - coupon_rate + sd^2/2)*expiration
-    d1 = d1./(sd*sqrt(expiration))
-    println(d1)
+    d1 = log(price/strike) + (riskfree_rate - coupon_rate + sd^2/2)*expiration;
+    d1 = d1./(sd*sqrt(expiration));
+    # println(d1)
     # d2 = d1 - sd*sqrt(expiration)
-    exp(-coupon_rate*expiration).*cdf(Normal(0, 1), d1)
+    exp(-coupon_rate*expiration).*cdf(Normal(0, 1), d1);
 end
 
 x1
 
 clean_colnames!(df)
+
+ncontracts = 1e5;
+rfr = 0.02;
+T = 0.25;                                # expiration
+nperiods = 50;
+dt = T/nperiods;
+strike = 50;
 prices = df["Stock_Price"]
-delta(prices[1:end-1], 50, 0.3, 0.25-[0:49]/200, 0.02, 0)*1e5
+optval = blackscholes(prices[1], strike, 0.3, 0.25, rfr, 0)*ncontracts;
+stock_holdings = delta(prices[1:end-1], strike, 0.3, 0.25-[0:49]/200, rfr, 0)*ncontracts
+stock_val = prices[1:end-1].*stock_holdings
+cash = zeros(length(stock_val),)
+portval = zeros(length(stock_val),)
+cash[1] = optval - stock_val[1]         # value of hedging portfolio
+portval[1] = optval
+for i = 1:(length(stock_val)-1)
+    portval[i+1] = cash[i]*exp(rfr*dt) + prices[i+1]*stock_holdings[i];
+    cash[i+1] = portval[i+1] - prices[i+1]*stock_holdings[i+1];
+end
 
+## Note that length of prices is one longer than cash and
+## stock_holdings so "end" does not refer to the same period
+finalportval = cash[end]*exp(rfr*dt) + prices[end]*stock_holdings[end];
+finaloptval = ncontracts*max(0, prices[end] - strike);
 
-z = [-0.99
--3.08
--0.50
-0.38
-1.51
-0.42
-0.82
-1.83
--0.46
-1.75
-1.23
-1.06
-1.29
--1.86
-1.58
--3.15
--1.46
-3.49
-1.93
--1.65
--1.92
--1.53
-3.05
--1.39
--1.40
-1.19
-0.89
-1.58
-1.48
--1.07
-1.97
-1.22
-2.66
--1.63
--0.80
-0.27
--0.80
--1.77
-0.41
-0.69
--2.31
--3.29
-0.72
-2.08
-0.75
--0.41
--1.90
--0.04
-4.08
-1.55]
+pandl = finalportval - finaloptval;
