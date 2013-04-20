1683692bc22913b9e0e5bd4b1d7a08901ea1ce2a
invalid <invalid@invalid>
2013-04-19 20:22:19 -0400

    hw8: replicating demo sheet

--- hw8/hw8.jl
+++ hw8/hw8.jl
@@ -2,14 +2,33 @@ Pkg.add("DataFrames")
 # Pkg.add("RDatasets")
 using DataFrames
 # using RDdatasets
+Pkg.add("Distributions")
+using Distributions
 
-sp = read_table("pset8.csv");            # stock prices
+sp = read_table("pset8.csv");            # simulated estimates of stock prices
 
 N = 50
 T = 0.25
 ## volatility
 x1 = sp[:,2]
 vol = round(100*sqrt(N/T)*std(log(x1[2:]./x1[1:end-1])),2)
+
+df = read_table("pset8-1.csv");            # actual stock holdings and prices
+
+function delta(price, strike, sd, expiration, riskfree_rate, coupon_rate)
+    d1 = log(price/strike) + (riskfree_rate - coupon_rate + sd^2/2)*expiration
+    d1 = d1./(sd*sqrt(expiration))
+    println(d1)
+    # d2 = d1 - sd*sqrt(expiration)
+    exp(-coupon_rate*expiration).*cdf(Normal(0, 1), d1)
+end
+
+x1
+
+clean_colnames!(df)
+prices = df["Stock_Price"]
+delta(prices[1:end-1], 50, 0.3, 0.25-[0:49]/200, 0.02, 0)*1e5
+
 
 z = [-0.99
 -3.08
